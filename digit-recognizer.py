d9097077fee4364bedfa4be4858dbee55b613141
DonghyunKim <leok0909@snu.ac.kr>
2021-04-03 19:11:37 +0900

    updated different digit classifier models

--- digit-recognizer.py
+++ digit-recognizer.py
@@ -2,20 +2,20 @@ import tensorflow as tf
 import numpy as np
 from scipy.io import loadmat
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, BatchNormalization, Dropout
 
 
 # lets train simple neural network model
 
 # first of all, we need data set for sudoku
 # svhn digits (for 0 ~ 9) + noise images (for None or blank cell) should work.
-def load_data(noise_level=10.):
+def load_data(noise_level=3.):
     train = loadmat('data/svhn_train_32x32')
     test = loadmat('data/svhn_test_32x32')
     x_train, y_train = train['X'], train['y']
     x_test, y_test = test['X'], test['y']
-    x_train = np.moveaxis(x_train, -1, 0) / 255.
-    x_test = np.moveaxis(x_test, -1, 0) / 255.
+    x_train = np.moveaxis(x_train, -1, 0)
+    x_test = np.moveaxis(x_test, -1, 0)
     y_train[y_train == 10] -= 10
     y_test[y_test == 10] -= 10
     y_train = y_train.squeeze()
@@ -40,13 +40,44 @@ def load_data(noise_level=10.):
     return (x_train_shuffled, y_train_shuffled), (x_test_shuffled, y_test_shuffled)
 
 # lets build model!
-def get_model(lr = 3e-4):
+# total params: 61,963
+def get_light_model(lr = 3e-4):
     model = Sequential([
+        Conv2D(32, 3, padding='same', activation='relu', input_shape=(32, 32, 3)),
+        BatchNormalization(),
+        MaxPooling2D(2),
+        Conv2D(64, 3, padding='same', activation='relu'),
+        BatchNormalization(),
+        Dropout(0.3),
+        MaxPooling2D(2),
+        Conv2D(32, 3, padding='same', activation='relu'),
+        BatchNormalization(),
+        Conv2D(32, 1, padding='same', activation='relu'),
+        Flatten(),
+        Dense(11, activation='softmax')
+    ])
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+# total_params: 314,571
+def get_heavy_model(lr = 3e-4):
+    model = Sequential([
+        Conv2D(32, 3, padding='same', activation='relu', input_shape=(32, 32, 3)),
+        BatchNormalization(),
         Conv2D(32, 3, padding='same', activation='relu', input_shape=(32, 32, 3)),
         MaxPooling2D(2),
         Conv2D(64, 3, padding='same', activation='relu'),
+        BatchNormalization(),
+        Dropout(0.3),
+        Conv2D(64, 3, padding='same', activation='relu'),
         MaxPooling2D(2),
-        Conv2D(16, 1, padding='same', activation='relu'),
+        Dropout(0.3),
+        Conv2D(128, 3, padding='same', activation='relu'),
+        BatchNormalization(),
+        Dropout(0.3),
+        Conv2D(128, 3, padding='same', activation='relu'),
+        Conv2D(32, 1, padding='same', activation='relu'),
         Flatten(),
         Dense(11, activation='softmax')
     ])
@@ -58,17 +89,17 @@ def get_model(lr = 3e-4):
 def train_model(file_path='weights/weights.h5', lr = 3e-5):
     check_point = tf.keras.callbacks.ModelCheckpoint(filepath=file_path)
     (x_train, y_train), (x_test, y_test) = load_data()
-    # model = get_model()
+    # model = get_light_model()
     model = tf.keras.models.load_model(filepath=file_path)
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr), loss='sparse_categorical_crossentropy',metrics=['accuracy'])
     model.fit(x=x_train, y=y_train, batch_size=128, epochs=15, verbose=1, validation_data=(x_test, y_test), callbacks=[check_point])
 
 
-train_model()
-
+# train_model()
 
 # want to test on empty cell?
-# model = tf.keras.models.load_model('weights/weights.h5')
-# none_img = np.full((1, 28, 28, 3), 0, dtype='uint8')
+# model = tf.keras.models.load_model('weights/weights_light_v1.h5')
+# none_img = np.full((1, 32, 32, 3), 0, dtype='uint8')
 # prediction = np.argmax(model.predict(none_img))
-# -> 10!
+# print(prediction)
+# # -> 10!
